6c21671669bdc3e5a4274dcc861c81831dce35df
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-08 16:06:27 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ def introduction(name)
 end  
   introduction("Josh")
   
-  def introduction(name,language)
+  def introduction_with_language(name,language)
     puts "Hi, my name is #{name} and I am learning to program #{language}."
   end
-    introduction("Josh","Ruby")
+    introduction_with_language("Josh","Ruby")
